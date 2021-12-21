74221ebc9fe8049ee257f6f2a09c08eb21d6577e
sameer <i.amsameer2061@gmail.com>
2021-12-21 16:47:01 +0545

    Create Server with env configuration

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Hello, world");
+import dotEnv from "dotenv";
+import app from "./app";
+
+// Configure Envirnoment Veriable
+dotEnv.config({ path: "./.env" });
+
+let port = process.env.PORT || 8000;
+let envirnoment = process.env.NODE_ENV;
+
+app.listen(port, console.log(`${envirnoment} server running on port:${port}`));
+
+process.on("uncaughtException", (err) => {
+  console.log("uncaught exception...");
+  console.log(err);
+  process.exit(1);
+});
