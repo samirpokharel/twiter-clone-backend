00ce7ff93bbcbc81f61d00bcac264c33301d06e0
sameer <i.amsameer2061@gmail.com>
2021-12-21 18:08:42 +0545

    Change database connection implementation and added nice colorful text login

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import dotEnv from "dotenv";
 import app from "./app";
-import "../Utils/DatabaseConnection";
+import { connect } from "mongoose";
+import "colors";
 
 // Configure Envirnoment Veriable
 dotEnv.config({ path: "./.env" });
+
+// Datbase Connections..
+const isDevelopment = process.env.NODE_ENV === "development";
+const DB_URL = isDevelopment ? process.env.DB_URL_DEV : process.env.DB_URL;
+
+connect(DB_URL)
+  .then((val) => console.log("Successfully! Connected to DB...".bgGreen))
+  .catch((err) => console.error(`Error: ${err}`.red));
+
+
+// Server setup
 
 let port = process.env.PORT || 8000;
 let envirnoment = process.env.NODE_ENV;
