9a7fa83fd7f75301fb7f97ab54eb72ef612a6b89
sameer <i.amsameer2061@gmail.com>
2021-12-22 15:09:56 +0545

    minor changes

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -6,11 +6,12 @@ import { Router } from "express";
 const router = Router();
 // Routes...
 router.post("/create-account", userCtrl.create_account);
-router.get("/conform-account", userCtrl.conform_account);
 router.post("/login-account", userCtrl.login_account);
 router.get("/current-account", protect, userCtrl.current_account);
 router.put("/update-account", protect, userCtrl.update_account);
 router.delete("/delete-account", protect, userCtrl.delete_account);
+
+router.get("/conform-account", userCtrl.conform_account);
 router.post("/forgot-account-password", userCtrl.forgot_account_password);
 router.put("/reset-account-password", userCtrl.reset_account_password);
 
